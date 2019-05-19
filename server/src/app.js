8039b73941d927016ba689c8fdb3640445158513
James Weir <james@weco.io>
2019-05-19 17:26:50 +0100

    Login page setup

--- server/src/app.js
+++ server/src/app.js
@@ -10,10 +10,10 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hello world!'
+        message: 'Hello ${req.body.email}! Your user was registered!'
     })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8082)
