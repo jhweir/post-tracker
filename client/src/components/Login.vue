f5355594c0f5141576ead5053a6be967377a444d
James Weir <james@weco.io>
2019-05-21 17:46:10 +0100

    Login updates

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,7 +3,7 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
@@ -26,9 +26,9 @@
 
           <v-btn
           class="cyan"
-          @click="register"
+          @click="login"
           dark>
-          Register
+          Login
           </v-btn>
 
         </div>
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -10,5 +10,37 @@ module.exports = {
         error: 'This email account is already in use'
       })
     }
+  },
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+
+      if (!user) {
+        res.status(403).send({
+          error: 'The login information was incorrect.'
+        })
+      }
+
+      const isPasswordValid = password === user.password
+      if (!isPasswordValid) {
+        return res.status(403).send({
+          error: 'The login information was incorrect.'
+        })
+      }
+      
+      const userJSON = user.toJSON()
+      res.send({
+        user: userJSON
+      })
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to login'
+      })
+    }
   }
 }
