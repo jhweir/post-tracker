00256f5793c5c99c8704631500cfdba8ef26cffe
James Weir <james@weco.io>
2019-05-21 21:21:12 +0100

    Jsonwebtoken setup

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
       host: process.env.DB_HOST || 'localhost',
       storage: './posttracker.sqlite'
     }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
   }
 }
